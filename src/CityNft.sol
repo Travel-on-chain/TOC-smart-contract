de69a3c5a12a19086d8a5c95bb0ce2a0107476c9
james.fan <james.fan@gaodun.com>
2024-05-12 21:35:22 +0800

    add cityNft V1

--- src/CityNft.sol
+++ src/CityNft.sol
@@ -1,63 +1,74 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
-import {VRFv2DirectFundingConsumer} from "../src/VRFv2DirectFundingConsumer.sol";
+
+struct UserCity {
+    string[] cityNftUserHaveMint;
+}
 
 contract CityNft is ERC721 {
-    string private s_shanghaiSvgUri1;
-    string private s_shanghaiSvgUri2;
-    string private s_beijingSvgUri1;
-    string private s_beijingSvgUri2;
-    VRFv2DirectFundingConsumer private s_randomWords;
-    //fixme 需要对接chainlink的随机数
     uint256 private s_cityIndex;
-
     uint256 private s_tokenCounter;
 
-    mapping(uint256 => CityNFTState) private s_tokenIdToState;
-    mapping(address => mapping(CityNFTState => uint256)) s_userCityCount;
+    //new state
     mapping(uint256 cityIndex => string nftAddress) private s_indexToNft;
+    mapping(address user => mapping(string country => string nowPosition)) s_userCountryPosition;
+    mapping(address user => mapping(string country => UserCity)) s_userCountryNft;
 
-    enum CityNFTState {
-        BEIJING,
-        SHANGHAI
-    }
+    event mintOwnNft(uint256 indexed s_tokenCounter);
 
-    constructor(
-        string memory shanghaiSvgUri1,
-        string memory shanghaiSvgUri2,
-        string memory beijingSvgUri1,
-        string memory beijingvgUri2,
-        uint256 randomNum
-    ) ERC721("CITY NFT", "CITYNFT") {
+    constructor() ERC721("CITY NFT", "CITYNFT") {
         s_tokenCounter = 0;
-        s_shanghaiSvgUri1 = shanghaiSvgUri1;
-        s_shanghaiSvgUri2 = shanghaiSvgUri2;
-        s_beijingSvgUri1 = beijingSvgUri1;
-        s_beijingSvgUri2 = beijingvgUri2;
-        s_cityIndex = randomNum;
     }
 
-    // 这里的mint逻辑 
-    // tokencounter-ipfs地址的映射
-    function mintNft(string nft_address) public {
+    function mintNft(
+        string memory countryName,
+        string memory cityName,
+        string memory nftAddress
+    ) public {
         uint256 tokenCounter = s_tokenCounter;
-        uint256 cityIndex = s_cityIndex;
         _safeMint(msg.sender, tokenCounter);
-        s_indexToNft[tokenCounter] = nft_address;
+        s_indexToNft[tokenCounter] = nftAddress;
         s_tokenCounter += 1;
-        CityNFTState city = CityNFTState(cityIndex);
 
-        //记录用户对于相同城市多次抽取次数的记录
-        s_userCityCount[msg.sender][city] == 0
-            ? 0
-            : s_userCityCount[msg.sender][city] + 1;
+        //更新用户当前位置
+        s_userCountryPosition[msg.sender][countryName] = cityName;
+
+        //更新用户在当前国家已拥有的nft
+        UserCity storage userCity = s_userCountryNft[msg.sender][countryName];
+        //must storage can use push
+        userCity.cityNftUserHaveMint.push(cityName);
+
+        // emit相应的事件出去
+        emit mintOwnNft(tokenCounter);
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "data:application/json;base64,";
+        return "https://aquamarine-fascinating-grouse-888.mypinata.cloud/ipfs/";
     }
 
     /**
@@ -67,51 +78,23 @@ contract CityNft is ERC721 {
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        //todo 通过tokenid 获取到对应IPFS的路径 映射给前端展示
-
-        //根据随机数的结果选择不同的城市
-        uint256 cityIndex = s_cityIndex;
-        CityNFTState city = CityNFTState(cityIndex);
-        string memory imageURI;
-
-        //  根据同一城市的去到的次数进行level的更新
-        uint256 cityLevel = s_userCityCount[msg.sender][city];
-
-        //pick city
-        if (CityNFTState.BEIJING == city) {
-            if (s_userCityCount[msg.sender][city] != 0) {
-                imageURI = s_beijingSvgUri2;
-                cityLevel = 1;
-            } else {
-                imageURI = s_beijingSvgUri1;
-                cityLevel = 0;
-            }
-        } else {
-            if (s_userCityCount[msg.sender][city] != 0) {
-                imageURI = s_shanghaiSvgUri2;
-                cityLevel = 1;
-            } else {
-                imageURI = s_shanghaiSvgUri1;
-                cityLevel = 0;
-            }
-        }
+        //通过tokenid 获取到对应IPFS的路径 映射给前端展示
+        string memory imageURI = string.concat(
+            _baseURI(),
+            s_indexToNft[tokenId]
+        );
 
         return
             string(
                 abi.encodePacked(
-                    _baseURI(),
-                    Base64.encode(
-                        bytes(
-                            abi.encodePacked(
-                                '{"name":"',
-                                name(),
-                                '", "description":"An NFT is describe China City, 100% on Chain!", ',
-                                '"attributes": [{"cityLevel": "',
-                                cityLevel,
-                                '", "value": 100000}], "image":"',
-                                imageURI,
-                                '"}'
-                            )
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(),
+                            '", "description":"An NFT is describe View of AllOverTheWorld!", ',
+                            '"attributes": ["image":"',
+                            imageURI,
+                            '"}'
                         )
                     )
                 )
@@ -120,5 +103,18 @@ contract CityNft is ERC721 {
 
     function getTokenCounter() public view returns (uint256) {
         return s_tokenCounter;
+    }
+
+    function getUserMintedCity(
+        string memory countryName
+    ) public view returns (string[] memory cityNftUserHaveMint) {
+        UserCity storage userCity = s_userCountryNft[msg.sender][countryName];
+        return userCity.cityNftUserHaveMint;
+    }
+
+    function getUserPosition(
+        string memory countryName
+    ) public view returns (string memory positonCity) {
+        return s_userCountryPosition[msg.sender][countryName];
     }
 }
