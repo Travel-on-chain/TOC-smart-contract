8d12c1a887a5f6c493e308a2041b946c109ecf3e
unic <2445942195@qq.com>
2024-05-23 10:22:15 +0800

    🗺️Limited Edition country NFT contract done.

--- src/LECountryNFT.sol
+++ src/LECountryNFT.sol
@@ -8,26 +8,43 @@ import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract LECountryNFT is ERC721, Ownable {
     error LECountryNFT__InvalidCountry(string country);
+    error LECountryNFT__MaximumQuantityReached(
+        string country,
+        uint16 maxSupply
+    );
     error LECountryNFT__OnlyOwnerOrCollateralOwner(address _opeartor);
 
-    event LECountryNFT__NFTMinted(uint256 indexed tokenCounter, address indexed owner, string indexed countryName);
+    event LECountryNFT__NFTMinted(
+        uint256 indexed tokenId,
+        address indexed owner,
+        string indexed countryName,
+        uint16 holderRank
+    );
 
     uint256 private s_tokenCounter;
+    uint16 private s_defaultMaxSupply;
     string[] private s_countries;
 
     mapping(uint256 nftIndex => string nftAddress) private s_indexToNft;
-    mapping(string => uint256) private s_maxSupplyCountryNFT;
-    mapping(string => uint256) private s_numCountryNFT;
+    mapping(string => uint16) private s_maxSupplyCountryNFT;
+    mapping(string => uint16) private s_numCountryNFT;
     mapping(address user => string[]) s_userCountriesNft;
 
-    constructor(string[] memory _countries) ERC721("Limited Edition Country NFT", "LECTNFT") Ownable(msg.sender) {
+    constructor(
+        string[] memory _countries,
+        uint16 _defaultMaxSupply
+    ) ERC721("Limited Edition Country NFT", "LECTNFT") Ownable(msg.sender) {
         s_tokenCounter = 0;
         s_countries = _countries;
+        s_defaultMaxSupply = _defaultMaxSupply;
+        setCountriesMaxSupply(_countries, _defaultMaxSupply);
     }
 
-    function isValidCountry(string memory _countryName) private view returns (bool valid) {
+    function isValidCountry(
+        string memory _countryName
+    ) private view returns (bool valid) {
         string[] memory countries = s_countries;
-        for (uint256 i = 0; i < s_countries.length; i++) {
+        for (uint256 i = 0; i < countries.length; i++) {
             bytes32 countriesHash = keccak256(abi.encodePacked(countries[i]));
             bytes32 countryNameHash = keccak256(abi.encodePacked(_countryName));
             if (countriesHash == countryNameHash) {
@@ -37,32 +54,56 @@ contract LECountryNFT is ERC721, Ownable {
     }
 
     // Need to update the following functions:
-    function mintNft(string memory _countryName, string memory nftAddress) public OnlyOwnerOrCollaborators {
+    function mintNft(
+        string memory _countryName,
+        string memory _nftAddress
+    ) public OnlyOwnerOrCollaborators {
         if (!isValidCountry(_countryName)) {
             revert LECountryNFT__InvalidCountry(_countryName);
         }
 
         uint256 tokenCounter = s_tokenCounter;
-        s_numCountryNFT[_countryName]++;
 
+        // Limit the number of NFTs to mint
+        if (
+            s_numCountryNFT[_countryName] >= s_maxSupplyCountryNFT[_countryName]
+        ) {
+            revert LECountryNFT__MaximumQuantityReached(
+                _countryName,
+                s_maxSupplyCountryNFT[_countryName]
+            );
+        }
+
+        s_numCountryNFT[_countryName]++;
         _safeMint(msg.sender, tokenCounter);
-        s_indexToNft[tokenCounter] = nftAddress;
+
+        s_indexToNft[tokenCounter] = _nftAddress;
         s_tokenCounter++;
 
         // 更新用户已拥有的国家 NFT
         string[] storage userCountries = s_userCountriesNft[msg.sender];
         userCountries.push(_countryName);
 
-        emit LECountryNFT__NFTMinted(tokenCounter, msg.sender, _countryName);
+        emit LECountryNFT__NFTMinted(
+            tokenCounter,
+            msg.sender,
+            _countryName,
+            s_numCountryNFT[_countryName]
+        );
     }
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://aquamarine-fascinating-grouse-888.mypinata.cloud/ipfs/";
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
         //通过 tokenid 获取到对应 IPFS 的路径 映射给前端展示
-        string memory imageURI = string.concat(_baseURI(), s_indexToNft[tokenId]);
+        string memory imageURI = string.concat(
+            _baseURI(),
+            s_indexToNft[tokenId]
+        );
         return string(abi.encodePacked(bytes(imageURI)));
     }
 
@@ -71,21 +112,45 @@ contract LECountryNFT is ERC721, Ownable {
         return s_tokenCounter;
     }
 
-    function getUserMintedCountries() public view returns (string[] memory countriesNftUserHave) {
+    function getUserMintedCountries()
+        public
+        view
+        returns (string[] memory countriesNftUserHave)
+    {
         return s_userCountriesNft[msg.sender];
     }
 
-    function getCountries() public view returns (string[] memory) {
+    function getCountries()
+        public
+        view
+        OnlyOwnerOrCollaborators
+        returns (string[] memory countries)
+    {
         return s_countries;
     }
 
     /* SETTER */
-    function setCountries(string[] memory _countries) public OnlyOwnerOrCollaborators {
+    function setCountries(
+        string[] memory _countries
+    ) public OnlyOwnerOrCollaborators {
         s_countries = _countries;
     }
 
-    function setCountryMaxSupply(string memory _country, uint256 _maxSupply) public OnlyOwnerOrCollaborators {
-        s_maxSupplyCountryNFT[_country] = _maxSupply;
+    function setCountriesMaxSupply(
+        string[] memory _countries,
+        uint16 _maxSupply
+    ) public OnlyOwnerOrCollaborators {
+        if (_countries.length > s_countries.length) {
+            revert LECountryNFT__InvalidCountry(
+                "New countries length is out of range"
+            );
+        }
+        for (uint i = 0; i < _countries.length; i++) {
+            if (!isValidCountry(_countries[i])) {
+                revert LECountryNFT__InvalidCountry(_countries[i]);
+            }
+            s_maxSupplyCountryNFT[_countries[i]] = _maxSupply;
+        }
     }
 
     /* Modifiers */
@@ -100,12 +165,12 @@ contract LECountryNFT is ERC721, Ownable {
         _;
     }
 
-    // 允许合约所有者添加合作伙伴
+    // Add a new collaborator.
     function addCollaborator(address _collaborator) external onlyOwner {
         s_collateralOwners[_collaborator] = true;
     }
 
-    // 允许合约所有者移除合作伙伴
+    // Remove an existing collaborator.
     function removeCollaborator(address _collaborator) external onlyOwner {
         s_collateralOwners[_collaborator] = false;
     }
